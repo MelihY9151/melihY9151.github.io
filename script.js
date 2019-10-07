bae82c80209c2ca30ce39efb31a3d3b494fc80f1
MelihY9151 <56261750+MelihY9151@users.noreply.github.com>
2019-10-07 09:37:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Melih Yumusak"
+let userName = "MelihY9151";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
